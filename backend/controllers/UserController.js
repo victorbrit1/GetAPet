5520d0b8028213160c532c959c2f46c8bf395a05
Victor Brito <rotcivbrito@outlook.com>
2025-07-20 18:46:52 -0300

    add verificação de token JWT

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -126,4 +126,10 @@ module.exports = class UserController{
         res.status(200).json({user})
     }
 
+    static async editUser(req,res){
+        res.status(200).json({message: 'Deu certo update!',})
+        return
+
+    }
+
 }
